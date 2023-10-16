71afdcb4e1b86d1b660dcdf897d67050ee23a3c8
José Bastos <josebezerrabastos@gmail.com>
2023-10-15 22:45:43 -0300

    aulas de variáveis

---
+++ aulas/src/edu/jose/variaveis/Variaveis.java
@@ -0,0 +1,14 @@
+package edu.jose.variaveis;
+
+public class Variaveis {
+    public static void main(String[] args) {
+        int numeroInteiro = 415;
+        short numeroCurto = 1;
+        byte numeroByte = 8;
+        double numeroDecimal = 4.500;
+        float numeroFlutuante = 1.50F;
+        long numeroLongo = 1039123L;
+
+        final int ano2000 = 2000;
+    }
+}
